770203b3e7bee5b7f629e20668fa3079664e8e48
Kerem <kerem.kurkcu@hotmail.com>
2022-03-05 15:51:17 +0300

    Add project files.

---
+++ ClassMethodDemo/CustomerManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMethodDemo
+{
+   class CustomerManager
+    {
+        public void Add(Customer customer)
+        {
+            Console.WriteLine(customer.Name + "isimli müşteri eklendi !");
+        }
+
+        public void Delete(Customer customer)
+        {
+            Console.WriteLine(customer.Name + "isimli müşteri silindi !");
+        }
+
+        public void List(Customer[] customers)
+        {
+            foreach (var customer in customers)
+            {
+                Console.WriteLine(customer.Id + " " + customer.Name + " " + customer.Surname + " " + customer.Age);
+            }
+        }
+    }
+}
---
+++ ClassMethodDemo/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ClassMethodDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer1 = new Customer();
+            customer1.Id = 1;
+            customer1.Name = "Kerem Can";
+            customer1.Surname = "Kürkçü";
+            customer1.Age = 21;
+
+            Customer customer2 = new Customer();
+            customer2.Id = 2;
+            customer2.Name = "Sebastian";
+            customer2.Surname = "Vettel";
+            customer2.Age = 34;
+
+            Customer customer33 = new Customer();
+            customer33.Id = 3;
+            customer33.Name = "Max";
+            customer33.Surname = "Verstappen";
+            customer33.Age = 24;
+        }
+    }
+}
